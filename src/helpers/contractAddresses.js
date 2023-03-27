985eeff743137414bba3aadb923f70c0b6e06990
OkekeChristian24 <christian.okeke24@gmail.com>
2023-03-27 02:48:18 -0700

    fix: update sale contract addresses

--- src/helpers/contractAddresses.js
+++ src/helpers/contractAddresses.js
@@ -1,3 +1,3 @@
-export const saleAddress = "0x170fb97Bc6Cddc32Bf4E2Dd5F8872A6dB8326047";
+export const saleAddress = "0xB502186e1958E3908423abA83c99552d304E1F3B";
 
 export const fswapAddress = "0x676d1c682c861226443A1302885Cc595Fd2b0A50";
